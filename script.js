c8a79a7f065223d0abba4e36dd878c9af8537bb7
xazkiel0 <xazkiel@gmail.com>
2023-09-26 13:55:56 +0700

    refactor: to bootstrap5

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const navbarMobileToggle = document.querySelector('#dd-navbar-toggle');
 const navbarMobileMenu = document.querySelector('#mobile-menu');
+const hamburgerIcon = document.querySelector('.hamburger-icon');
 
 navbarMobileToggle.addEventListener('click', () => {
   navbarMobileMenu.classList.toggle('hidden');
+  hamburgerIcon.classList.toggle('fa-xmark');
 });
 
 console.log('script.js loaded');
