d1fe379fcfe608150967bae50bb89ed8ca88fd2c
xazkiel0 <xazkiel@gmail.com>
2023-10-02 13:48:13 +0700

    feat: add slider to product and add faq section

--- script.js
+++ script.js
@@ -1,10 +1,51 @@
-const navbarMobileToggle = document.querySelector('#dd-navbar-toggle');
-const navbarMobileMenu = document.querySelector('#mobile-menu');
-const hamburgerIcon = document.querySelector('.hamburger-icon');
+const sliderWrapper = document.querySelector('.slider-wrapper');
+const body = document.querySelector('body');
+const navSlide = document.querySelectorAll('.nav-slide');
+const cardProductWidth = sliderWrapper.querySelector('.card-product').offsetWidth;
 
-navbarMobileToggle.addEventListener('click', () => {
-  navbarMobileMenu.classList.toggle('hidden');
-  hamburgerIcon.classList.toggle('fa-xmark');
-});
+let isDragging = false, startX, startScrollLeft;
 
-console.log('script.js loaded');
+
+navSlide.forEach((navBtn) => {
+  console.log(navBtn);
+  navBtn.addEventListener('click', () => {
+    console.log(navBtn.id);
+    sliderWrapper.scrollLeft += navBtn.id === 'next-btn' ? cardProductWidth : -cardProductWidth;
+  })
+})
+
+const dragStart = e => {
+  isDragging = true;
+  sliderWrapper.classList.add('dragging');
+  startX = e.pageX;
+  startScrollLeft = sliderWrapper.scrollLeft;
+  console.log(startX, startScrollLeft);
+}
+
+const dragEnd = (e) => {
+  isDragging = false;
+  sliderWrapper.classList.remove('dragging');
+
+}
+
+const dragging = e => {
+  if (!isDragging) return;
+
+  sliderWrapper.scrollLeft = startScrollLeft - (e.pageX - startX);
+
+
+}
+
+sliderWrapper.addEventListener('mousedown', dragStart);
+sliderWrapper.addEventListener('mousemove', dragging);
+body.addEventListener('mouseup', dragEnd);
+
+
+function scrollTo(_el) {
+  const el = document.getElementById(_el);
+
+  if (el) {
+    el.scrollIntoView({ behavior: 'smooth', block: 'start' });
+  }
+
+}
