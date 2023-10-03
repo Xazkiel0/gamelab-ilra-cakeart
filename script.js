a691e21f04b06066c3d1a1e879bd810c009f1baf
xazkiel0 <xazkiel@gmail.com>
2023-10-04 06:40:45 +0700

    feat: animation

--- script.js
+++ script.js
@@ -1,65 +1,10 @@
-const sliderWrapper = document.querySelector('.slider-wrapper');
-const body = document.querySelector('body');
-const navSlide = document.querySelectorAll('.nav-slide');
-const cardProductWidth = sliderWrapper.querySelector('.card-product').offsetWidth;
+const navbar = document.getElementById('navbar');
+const endOfHero = document.getElementById('endOfHero');
 
-let isDragging = false, startX, startScrollLeft;
-
-function productSlideBtnOff(scrollLeft) {
-  if (scrollLeft <= 5) {
-    document.getElementById('prev-btn').classList.add('off');
+window.addEventListener('scroll', () => {
+  if (window.scrollY >= 1) {
+    navbar.classList.add('bg-primary');
   } else {
-    document.getElementById('prev-btn').classList.remove('off');
+    navbar.classList.remove('bg-primary');
   }
-  if (scrollLeft >= sliderWrapper.scrollWidth - sliderWrapper.offsetWidth - 5) {
-    document.getElementById('next-btn').classList.add('off');
-  }
-  else {
-    document.getElementById('next-btn').classList.remove('off');
-  }
-}
-productSlideBtnOff(sliderWrapper.scrollLeft);
-
-navSlide.forEach((navBtn) => {
-
-  navBtn.addEventListener('click', () => {
-    if (navBtn.classList.contains('off')) return;
-    sliderWrapper.scrollLeft += navBtn.id === 'next-btn' ? cardProductWidth : -cardProductWidth;
-    productSlideBtnOff(sliderWrapper.scrollLeft);
-  })
-})
-
-const dragStart = e => {
-  isDragging = true;
-  sliderWrapper.classList.add('dragging');
-  startX = e.pageX;
-  startScrollLeft = sliderWrapper.scrollLeft;
-}
-
-const dragEnd = (e) => {
-  isDragging = false;
-  sliderWrapper.classList.remove('dragging');
-  productSlideBtnOff(sliderWrapper.scrollLeft);
-}
-
-const dragging = e => {
-  if (!isDragging) return;
-
-  sliderWrapper.scrollLeft = startScrollLeft - (e.pageX - startX);
-
-
-}
-
-sliderWrapper.addEventListener('mousedown', dragStart);
-sliderWrapper.addEventListener('mousemove', dragging);
-body.addEventListener('mouseup', dragEnd);
-
-
-function scrollTo(_el) {
-  const el = document.getElementById(_el);
-
-  if (el) {
-    el.scrollIntoView({ behavior: 'smooth', block: 'start' });
-  }
-
-}
+});
