cad5f3d4693321c0a5b902dd9dfe2762f41fae60
xazkiel0 <xazkiel@gmail.com>
2023-10-13 15:35:05 +0700

    feat: carousel hero

--- script.js
+++ script.js
@@ -1,6 +1,109 @@
 const navbar = document.getElementById('navbar');
 const endOfHero = document.getElementById('endOfHero');
 
+
+let cakes = {
+  popularCakes: [
+    {
+      imgPath: 'all_cakes/39bf661e-bd42-44e7-baed-2cf007b867d1.jpeg',
+      name: 'Sakura Cake',
+      description: 'kue dengan dekorasi bunga sakura yang cantik diatasnya dengan tambahan batang pohon dari coklat.',
+    },
+    {
+      imgPath: 'all_cakes/842e12cd-5167-49d6-85b7-90f12a3df6a5.jpeg',
+      name: 'Jelly Art Cake',
+      description: 'kue dengan jelly sebagai bahan utama yang bisa juga dibuat sebagai tema laut atau rawa.',
+    },
+    {
+      imgPath: 'all_cakes/4.jpeg',
+      name: 'Romantic Cake',
+      description: 'kue tema romantis yang biasa dibuat untuk acara pernikahan atau anniversary.',
+    },
+    {
+      imgPath: 'all_cakes/5.jpeg',
+      name: 'Books Cake',
+      description: 'kue dengan tema buku yang biasa dibuat untuk teman atau keluarga yang menyukai buku.',
+    },
+    {
+      imgPath: 'all_cakes/6.jpeg',
+      name: 'Car-themed Cake',
+      description: 'kue dengan tema mobil yang biasa dibuat untuk anak-anak yang menyukai mobil.',
+    },
+    {
+      imgPath: 'all_cakes/7.jpeg',
+      name: 'Anniversary Cake',
+      description: 'kue dengan tema anniversary yang biasa dibuat khusus untuk acara anniversary.',
+    },
+    {
+      imgPath: 'all_cakes/8.jpeg',
+      name: 'Rustic Cake',
+      description: 'Kue rustic bisa menjadi pilihan yang tepat untuk acara yang bertemakan natural yang sempurna untuk segala suasana.',
+    },
+    {
+      imgPath: 'all_cakes/9.jpeg',
+      name: 'Beach-themed Cake',
+      description: 'kue yang dihias dengan tema pantai. Kue ini biasanya dibuat dengan warna-warna cerah yang menggambarkan suasana pantai.'
+    },
+    {
+      imgPath: 'all_cakes/10.jpeg',
+      name: 'Flower Cake',
+      description: 'kue dengan hiasan berbagai macam bentuk dan warna bunga dengan tampilan cantik dan elegan.'
+    },
+  ],
+  kidCakes: [
+    {
+      imgPath: 'all_cakes/hello_kitty/Hellokitty4.jpg',
+      name: 'Hello Kitty',
+      description: 'Kue ini dihias dengan karakter Hello Kitty yang lucu dan menggemaskan.'
+    },
+    {
+      imgPath: 'all_cakes/car/cartheme1.jpg',
+      name: 'Car-themed Cake',
+      description: 'kue dengan tema mobil yang biasa dibuat untuk anak-anak yang menyukai mobil.'
+    },
+    {
+      imgPath: 'all_cakes/construct/theme1.jpg',
+      name: 'Construct-themed Cake',
+      description: 'kue yang dihias dengan tema konstruksi seperti buldozer atau alat-alat berat yang biasa dilihat anak laki-laki.'
+    },
+    {
+      imgPath: 'all_cakes/frock_cake/Frock1.jpg',
+      name: 'Frock Cake',
+      description: 'Kue ini dihias dengan gaun putri yang cantik dan anggun untuk anak perempuan yang menyukai karakter princess.'
+    }
+  ],
+  cheesecakes: [
+    {
+      imgPath: 'all_cakes/new_york/cheesecake1.jpg',
+      name: 'New York Cheesecake',
+      description: 'dessert yang lembut dan creamy, dengan rasa keju yang manis dan gurih.'
+    },
+    {
+      imgPath: 'all_cakes/strawberrywhipcream/cheesecake1.jpg',
+      name: 'Strawberry Whipcream Cheesecake',
+      description: 'dessert yang dihias dengan saus stroberi dan krim kocok dengan perpaduan rasa manis dan asam yang menyegarkan.'
+    },
+    {
+      imgPath: 'all_cakes/basque_burn/cheesecake1.jpg',
+      name: 'Basque Burn Cheesecake',
+      description: 'dessert yang dipanggang pada suhu tinggi hingga bagian luarnya hangus dan legit dengan tekstur yang lembut dan creamy di bagian dalam.'
+    },
+  ],
+  noCakes: [],
+}
+document.addEventListener('alpine:init', () => {
+  Alpine.data('products_data', () => ({
+    products: cakes.popularCakes,
+
+    swapCake(_cakes) {
+      this.products = [];
+      setTimeout(() => {
+        this.products = _cakes
+      }, 500);
+    }
+  }))
+})
+
 document.addEventListener('DOMContentLoaded', () => {
   navbarColChange();
 });
